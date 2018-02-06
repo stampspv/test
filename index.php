99d63426f246582d37313a235506e1d3499cc946
stampspv <suphavich.c@gmail.com>
2018-02-06 20:41:10 +0700

    test
    test

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+	<!-- Latest compiled and minified CSS & JS -->
+	<link href="https://fonts.googleapis.com/css?family=Itim" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<div class="row">
+  <div class="col-md-2">
+  	
+  </div>
+  <!-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ -->
+  <div class="col-md-8">
+  	Test
+  </div>
+  <div class="col-md-2">
+  	
+  </div>
+</body>
+</html>
